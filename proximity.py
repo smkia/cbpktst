0b40e54289afaef2eaf7c1fa02aad0b32ab49215
Emanuele Olivetti <olivetti@fbk.eu>
2014-07-08 10:25:07 +0200

    added computation of sparse boolean proximity matrix in space and time + docstring improved

--- proximity.py
+++ proximity.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, lil_matrix
 # from scipy.spatial.kdtree import KDTree
 from sklearn.neighbors import KDTree
 from sklearn.metrics import pairwise_distances
@@ -8,6 +8,9 @@ from sklearn.metrics import pairwise_distances
 def compute_boolean_proximity_matrix(coordinates, threshold):
     """Compute the boolean proximity matrix of units (with given
     coordinates) which have Euclidean distance less than threshold.
+
+    coordinates is a matrix where each row is the vector of
+    coordinates of a unit/sensor.
     """
     dm = pairwise_distances(coordinates)
     proximity_matrix = dm < threshold
@@ -37,4 +40,58 @@ def compute_sparse_boolean_proximity_matrix(coordinates, threshold):
     column = np.concatenate(neighbors.tolist())
     data = np.ones(len(row), dtype=np.bool)
     proximity_matrix = coo_matrix((data, (row, column)), shape=(coordinates.shape[0], coordinates.shape[0]), dtype=np.bool)
+    return proximity_matrix.tocsc()
+
+
+def compute_sparse_boolean_proximity_matrix_space_time(coordinates, n_timesteps, threshold_space=0.1, threshold_timesteps=1, space_sparse=False):
+    """Create the proximity matrix of a set of units with given
+    coordinates in space and time, where two units are proximal if
+    their Euclidean distance is less then the given threshold_space
+    and their linear distance is less then the given threshold_time.
+
+    In practice we create a proximity matrix where each node is one
+    sensor at one specific timestep. Two sensors (irrespective of
+    time) are proximal if their Euclidean distance is less than
+    threshold_space. Moreover the same sensor is proximal to itself at
+    a different timesteps is the time distance is equal or less then
+    threshold_timesteps. Since the proximity matrix becomes huge,
+    i.e. ((n_units * n_timesteps) , (n_units * n_timesteps)), but
+    sparse, we build it as a scipy.sparse.lil_matrix. The matrix is
+    constructed by replicating the spatial proximity matrix on the
+    diagonal a number of times equal to the number of timesteps. The
+    time proximity is encoded with (or more) offset diagonal(s) set to
+    True.
+
+    The ordered list of nodes on the rows (and equally on the columns)
+    is:
+    ch0[t=0],...,chN[t=0],ch0[t=1],...,chN[t=1],...,ch0[t=M],...,chN[t=M]
+    where N is the number of units (n_units) and M is the number
+    of timesteps (n_timesteps).
+
+    Note: lil_matrix is the necessary format to allow fancy indexing
+    when patching the spatial proximity matrix into the proximity
+    matrix and to set the diagonals. But in order to extract
+    submatrices in ktst_map.compute_clusters_statistic(), it is
+    necessary to convert the lil_matrix into a csc_matrix.
+    """
+    # compute the space proximity matrix:
+    if space_sparse:
+        proximity_matrix_space = compute_sparse_boolean_proximity_matrix(coordinates, threshold_space)
+    else:
+        proximity_matrix_space = compute_boolean_proximity_matrix(coordinates, threshold_space)
+
+    # Create the very large sparse space time proximity matrix:
+    n_units = len(coordinates)
+    proximity_matrix = lil_matrix((n_units * n_timesteps, n_units * n_timesteps), dtype=np.bool)
+
+    # Fill the sparse matrix with the space proximity matrix as a
+    # block on the diagonal:
+    for i in range(n_timesteps):
+        proximity_matrix[i*n_units:(i+1) * n_units, i*n_units:(i+1) * n_units] = proximity_matrix_space
+
+    # Fill the offset diagonals with 'True' to encode proximity in time.
+    for k in range(1, threshold_timesteps + 1):
+        proximity_matrix.setdiag(np.ones(n_units * (n_timesteps - k), dtype=np.bool), n_units * k)
+        proximity_matrix.setdiag(np.ones(n_units * (n_timesteps - k), dtype=np.bool), - n_units * k)
+
     return proximity_matrix.tocsc()
