4ec6ca67d8f3915fe98f63f93903e3007e004228
Emanuele Olivetti <olivetti@fbk.eu>
2014-07-30 18:20:45 +0200

    added new simulated dataset + minor fix

--- simulate_data.py
+++ simulate_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from proximity import compute_boolean_proximity_matrix, compute_sparse_boolean_proximity_matrix
 
-def simulate_2d(d=20, k=10, m=200, n=200, threshold=1.0):
+def simulate_2d(d=20, k=10, m=200, n=200, threshold=1.0, seed=0):
     """This data simulator generates two samples, of m and n trials
     each. Each trial consists of measurements on a 2D grid of (k x k)
     sensors (units). Each measurement is a d-dimensional vector. By
@@ -17,6 +17,28 @@ def simulate_2d(d=20, k=10, m=200, n=200, threshold=1.0):
     sigma2_spatial = (np.sqrt(((coordinates + 1.0)**2).sum(1)) * 50.0)
     XX = np.array([np.random.multivariate_normal(np.zeros(d), s2 * np.eye(d), size=m) for s2 in sigma2_spatial]).transpose(1,0,2)
     YY = np.array([np.random.multivariate_normal(np.ones(d), s2 * np.eye(d), size=n) for s2 in sigma2_spatial]).transpose(1,0,2)
+    # proximity_matrix = compute_boolean_proximity_matrix(coordinates, threshold=threshold) # this is a dense matrix, which is OK for low k
+    proximity_matrix = compute_sparse_boolean_proximity_matrix(coordinates, threshold=threshold) # this is a sparse matrix which is good for large k
+    print("%f of the pairs of units are proximal." % ((len(proximity_matrix.nonzero()[0]) - n_units) / 2.0  / (n_units * (n_units - 1.0) / 2.0)))
+
+    return XX, YY, proximity_matrix, coordinates
+
+
+def simulate_2d_distance(d=20, k=10, m=200, n=200, threshold=1.0, seed=0):
+    """This data simulator generates two samples, of m and n trials
+    each. Each trial consists of measurements on a 2D grid of (k x k)
+    sensors (units). Each measurement is a d-dimensional vector. By
+    design, the sensors (units) close to location (0,0) show a strong
+    difference between the two samples. Those far from (0,0) show
+    almost no difference between the two samples.
+    """
+    print("Creating simulated data.")
+    n_units = k * k
+    coordinates_2D = np.meshgrid(np.arange(k), np.arange(k))
+    coordinates = np.array(zip(coordinates_2D[0].flat,coordinates_2D[1].flat))
+    distances = (np.sqrt(((coordinates + 1.0)**2).sum(1)) / 150.0)
+    XX = np.array([np.random.multivariate_normal(np.zeros(d), np.eye(d), size=m) for distance in distances]).transpose(1,0,2)
+    YY = np.array([np.random.multivariate_normal(distance * np.ones(d), np.eye(d), size=n) for distance in distances]).transpose(1,0,2)
     # proximity_matrix = compute_boolean_proximity_matrix(coordinates, threshold=threshold) # this is a dense matrix, which is OK for low k
     proximity_matrix = compute_sparse_boolean_proximity_matrix(coordinates, threshold=threshold) # this is a sparse matrix which is good for large k
     print("%f of the pairs of units are proximal." % ((len(proximity_matrix.nonzero()[0]) - n_units) / 2.0  / (n_units * (n_units - 1.0) / 2.0)))
@@ -40,7 +62,8 @@ def plot_map2d(k, coordinates, unit_statistic_homogeneous_significant, vmin=None
 
 if __name__ == '__main__':
 
-    XX, YY, proximity_matrix = simulate_2d()
+    XX, YY, proximity_matrix, coordinates = simulate_2d()
+    XX, YY, proximity_matrix, coordinates = simulate_2d_distance()
 
 
 
